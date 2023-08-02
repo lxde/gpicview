1c0c9211999696da4709ec26eff9486459e7bf9c
Ingo Brückl <ib@oddnet.de>
2023-08-02 14:02:39 +0200

    Update copyright information

--- src/gpicview.c
+++ src/gpicview.c
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2007 by PCMan (Hong Jen Yee) <pcman.tw@gmail.com>       *
+ *   Copyright (C) 2023 by Ingo Brückl                                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
