lxde/gpicview

commit ceaa296e3208b77ab23d1f48403e2b3f4e97955f

Scroll to cursor position when zooming
The image is zoomed to the area under the mouse pointer
when it no longer fits in the window.

This fixes https://sourceforge.net/p/lxde/bugs/108.

Patch by Flavius, https://sourceforge.net/p/lxde/patches/563.

src/image-view.c
@@ -357,10 +357,26 @@ void image_view_set_scale( ImageView* iv, gdouble new_scale, GdkInterpType type 
     if( new_scale == iv->scale )
         return;
 //    gdouble factor = new_scale / iv->scale;
+
+    gint xPos, yPos;
+    gtk_widget_get_pointer(GTK_WIDGET(iv), &xPos, &yPos);
+
+    gdouble oldRelativePositionX =  xPos * 1.0 / iv->img_area.width;
+    gdouble oldRelativePositionY = yPos * 1.0 / iv->img_area.height;
+    gdouble visibleAreaX = xPos - gtk_adjustment_get_value(iv->hadj);
+    gdouble visibleAreaY = yPos - gtk_adjustment_get_value(iv->vadj);
+
     iv->scale = new_scale;
     if( iv->pix )
     {
         calc_image_area( iv );
+
+        gdouble newPosX = oldRelativePositionX * iv->img_area.width - visibleAreaX;
+        gdouble newPosY = oldRelativePositionY * iv->img_area.height - visibleAreaY;
+
+        iv->hadj->value = newPosX > 0 ? newPosX : 0;
+        iv->vadj->value = newPosY > 0 ? newPosY : 0;
+
         gtk_widget_queue_resize( (GtkWidget*)iv );
 //        gtk_widget_queue_draw( (GtkWidget*)iv );
 

commit 7931f2325f7b56639bb5c5f02cd2f26a360002bc

Fix compilation with GTK+ 3
This has been broken in commit ceaa296e3208b77ab23d1f48403e2b3f4e97955f
and commit 46e85278c686ddf8306fb9ba8ef3493bb71ac9a9.

src/image-view.c
@@ -365,8 +365,8 @@ void image_view_set_scale( ImageView* iv, gdouble new_scale, GdkInterpType type 
     gdouble vadj_value = gtk_adjustment_get_value(iv->vadj);
 
     // if the pointer is not inside the window...
-    if (xPos < hadj_value || xPos > hadj_value + iv->hadj->page_size ||
-        yPos < vadj_value || yPos > vadj_value + iv->vadj->page_size)
+    if (xPos < hadj_value || xPos > hadj_value + gtk_adjustment_get_page_size(iv->hadj) ||
+        yPos < vadj_value || yPos > vadj_value + gtk_adjustment_get_page_size(iv->vadj))
     {
         // ...pretend that it is over the center of the image
         xPos = iv->img_area.width / 2;
@@ -386,8 +386,14 @@ void image_view_set_scale( ImageView* iv, gdouble new_scale, GdkInterpType type 
         hadj_value = oldRelativePositionX * iv->img_area.width - visibleAreaX;
         vadj_value = oldRelativePositionY * iv->img_area.height - visibleAreaY;
 
+#if GTK_CHECK_VERSION(3, 0, 0)
+        gtk_adjustment_set_value(iv->hadj, hadj_value > 0 ? hadj_value : 0);
+        gtk_adjustment_set_value(iv->vadj, vadj_value > 0 ? vadj_value : 0);
+#else   // gtk_adjustment_set_value() triggers a "value-changed" event,
+        // causing image flickering in GTK+ 2 due to multiple paint calls
         iv->hadj->value = hadj_value > 0 ? hadj_value : 0;
         iv->vadj->value = vadj_value > 0 ? vadj_value : 0;
+#endif
 
         gtk_widget_queue_resize( (GtkWidget*)iv );
 //        gtk_widget_queue_draw( (GtkWidget*)iv );